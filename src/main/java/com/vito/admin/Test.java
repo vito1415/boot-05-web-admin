3d1bc61e43efafc5d49461d50952dd664b6ccacf
vito <liangzzz147@163.com>
2023-02-14 22:47:16 +0800

    Update Test.java
    a

--- src/main/java/com/vito/admin/Test.java
+++ src/main/java/com/vito/admin/Test.java
@@ -3,5 +3,6 @@ package com.vito.admin;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello 222222");
     }
 }
