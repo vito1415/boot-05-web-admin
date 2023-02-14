af66d721043a3ada44d59e90e6b9e38bba112450
lzj <test@qq.com>
2023-02-14 22:18:30 +0800

    idea commit all

---
+++ src/main/java/com/vito/admin/Boot05WebAdminApplication.java
@@ -0,0 +1,15 @@
+package com.vito.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan(basePackages = "com.vito.admin.servlet")
+@SpringBootApplication
+public class Boot05WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05WebAdminApplication.class, args);
+    }
+
+}
